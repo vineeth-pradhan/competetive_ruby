cb2d35090494808a135d513c27829bbfb46e0c45
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-04-04 18:29:53 +0530

    Better time complexity

--- leetcode/reverse.rb
+++ leetcode/reverse.rb
@@ -3,25 +3,20 @@
 # @param {Integer} x
 # @return {Integer}
 def reverse(x)
-  @result = []
-  multiplier = 1
-  unless x.positive?
-    multiplier = -1
-    x = x * -1
-  end
-
-  rev(x)
-  p @result.join('').to_i * multiplier
+  arr = []
+  sign = x.positive? ? 1 : -1
+  return x * sign if x/10 == 0
+  return 0 if(x > ((2 ** 31) - 1) || x <= -((2 ** 31/2)))
+  rev(x*sign, arr) * sign
 end
 
-def rev(x)
-  if(x%10 == x)
-    @result.push(x)
-    @result
-  else
-    @result.push(x%10)
-    rev(x/10)
+def rev(x,arr)
+  arr << x % 10 unless (x/10).positive?
+  if (y=x/10).positive?
+    arr << x % 10
+    rev(y,arr)
   end
+  arr.join.to_i
 end
 
-reverse(1534236469)
+p reverse(1534236469)
