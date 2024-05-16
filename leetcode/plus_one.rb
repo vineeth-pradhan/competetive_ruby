f1f362f583325d80368dca5e2e771939c07934c0
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-16 20:01:54 +0530

    Add last digit

---
+++ leetcode/plus_one.rb
@@ -0,0 +1,15 @@
+class Solution
+  def plus_one(digits)
+    result = []
+    idx = digits.size-1
+    carry_over = 0
+    while(idx >= 0)
+      sum = (idx == digits.size - 1) ? (digits[idx] + 1 + carry_over) : (digits[idx] + carry_over)
+      carry_over = sum / 10
+      result.unshift(sum % 10)
+      idx -= 1
+    end
+    result.unshift(carry_over) if carry_over != 0
+    result
+  end
+end
