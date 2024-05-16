614109585ae1f5f68f941890394d7b9725d1aebc
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-16 14:29:18 +0530

    Handling corner cases

--- leetcode/reverse_linked_list.rb
+++ leetcode/reverse_linked_list.rb
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/reverse-linked-list/
 #
 class Node
-  attr_accessor :data, :nextt
+  attr_accessor :data, :next
 
-  def initialize(data=nil,nextt=nil)
+  def initialize(data=nil,_next=nil)
     self.data = data
-    self.nextt = nextt
+    self.next = _next
   end
 end
 
@@ -14,41 +14,49 @@ class Solution
 
   def initialize(array)
     self.head = Node.new
-    self.head.nextt = Node.new
-    create_nodes(0,array,self.head.nextt) unless array.empty?
+    create_nodes(0,array,self.head) unless array.empty?
   end
 
-  def print_all(node)
-    if node && node.data
-      print node.data
-      print '->' if node.nextt
-      print_all(node.nextt) if node.nextt
-    end
+  def print_list
+    node = self.head
+    helper(node)
   end
 
   def reverse
-    helper(self.head.nextt, self.head.nextt.nextt, self.head)
+    traverse_helper(self.head, self.head.next, self.head)
   end
 
   private
 
-  def helper(current, _next, head)
-    if(_next.nextt)
-      temp = _next.nextt
-      _next.nextt = current
-      current.nextt = nil if head.nextt == current
-      helper(_next, temp, head)
+  def helper(node)
+    if node
+      print node.data
+      if node.next
+        print '->'
+        helper(node.next)
+      end
+    end
+  end
+
+  def traverse_helper(current, _next, head)
+    current.next = nil if head == current
+    if(_next.next)
+      temp = _next.next
+      _next.next = current
+      traverse_helper(_next, temp, head)
     else
-      _next.nextt = current
+      _next.next = current
       self.head = _next
     end
   end
 
-  def create_nodes idx, array, node
+  def create_nodes(idx, array, node)
     if idx < array.size
       node.data = array[idx]
-      node.nextt = Node.new
-      create_nodes idx+1, array, node.nextt
+      if idx+1 < array.size
+        node.next = Node.new
+        create_nodes(idx+1, array, node.next)
+      end
     end
   end
 end
