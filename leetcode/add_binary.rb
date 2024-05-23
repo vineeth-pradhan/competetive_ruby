443aab348f7fcda4d89400f4b0f7f64413811515
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-23 07:30:26 +0530

    Try: Better time complexity

--- leetcode/add_binary.rb
+++ leetcode/add_binary.rb
@@ -1,5 +1,18 @@
 class Solution
   def add_binary(a, b)
-    (a.to_i(2) + b.to_i(2)).to_s(2)
+    a.size >= b.size ? (max = a; min = b) : (max = b; min = a)
+    i = 0
+    result = ""
+    carry = 0
+    while(i < max.size)
+      sum = (max[max.size - i - 1].to_i + min[min.size - i - 1].to_i + carry) % 2
+      carry = (max[max.size - i - 1].to_i + min[min.size - i - 1].to_i + carry) / 2
+      result.prepend(sum.to_s)
+      i += 1
+    end
+    # if carry != 0
+    #   result.prepend(carry.to_s)
+    # end
+    result
   end
 end
