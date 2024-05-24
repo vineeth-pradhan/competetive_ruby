cdbc527b3114d3dfe546bc92b2cb9209932657cc
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-24 06:11:25 +0530

    Better time complexity

--- leetcode/add_binary.rb
+++ leetcode/add_binary.rb
@@ -5,14 +5,19 @@ class Solution
     result = ""
     carry = 0
     while(i < max.size)
-      sum = (max[max.size - i - 1].to_i + min[min.size - i - 1].to_i + carry) % 2
-      carry = (max[max.size - i - 1].to_i + min[min.size - i - 1].to_i + carry) / 2
+      if min.size - i - 1 >= 0
+        sum = (max[max.size - i - 1].to_i + min[min.size - i - 1].to_i + carry) % 2
+        carry = (max[max.size - i - 1].to_i + min[min.size - i - 1].to_i + carry) / 2
+      else
+        sum = (max[max.size - i - 1].to_i + carry) % 2
+        carry = (max[max.size - i - 1].to_i + carry) / 2
+      end
       result.prepend(sum.to_s)
       i += 1
     end
-    # if carry != 0
-    #   result.prepend(carry.to_s)
-    # end
+    if carry != 0
+      result.prepend(carry.to_s)
+    end
     result
   end
 end
